f69e3139cd72712980bee52daca751daa41c2787
subash <subash43it@proton.me>
2024-08-16 22:35:00 +0530

    first number variable

--- firsttime/hello.java
+++ firsttime/hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        String name = "Subash, Ram, Laksh, Kumar";
-        System.out.println(name);
+        int number = 223;
+        System.out.println(number);
     }
 }
