88c9c2fe3588cd1bc6d7490212e407694df22724
subash <subash43it@proton.me>
2024-08-18 10:27:31 +0530

    Nested Array  length finding with for each loop.

--- firsttime/hello.java
+++ firsttime/hello.java
@@ -1,9 +1,11 @@
 public class hello {
     public static void main(String [] args) {
-        String[] name = { "Dogs", "Cats", "Birds" };
+        String[][] name = { {"Dogs", "Cats", "Birds"}, {"rabbit", "cow", "lion"} }; 
 
-        for (String species : name) {
-            System.out.println(species.length());
+        for (String[] innerArray : name) {  
+            for (String species : innerArray) { 
+                System.out.println(species.length());
+            }
         }
     }
 }
