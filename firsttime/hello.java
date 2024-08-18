9d062c8f340c84660d24fe49a5777a9cba5f91c4
subash <subash43it@proton.me>
2024-08-18 10:16:25 +0530

    Looping Array

--- firsttime/hello.java
+++ firsttime/hello.java
@@ -1,6 +1,9 @@
 public class hello {
-    public static void main(String[] args) {
-        int number = 223;
-        System.out.println(number);
+    public static void main(String [] args) {
+        String[] name = { "Dogs", "Cats", "Birds" };
+
+        for (String species : name) {
+            System.out.println(species.length());
+        }
     }
 }
