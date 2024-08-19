2d266af114c743d3a108e53cc56b01472d0892b8
subash <subash43it@proton.me>
2024-08-19 08:36:32 +0530

    multi object creating accessing the object

--- firsttime/Main.java
+++ firsttime/Main.java
@@ -1,8 +1,9 @@
 public class Main {
-  int x = 55;
+  int x = 20;
+  int y = 40;
 
-  public static void main(String[] args) {
-    Main myObj = new Main();
-    System.out.println(myObj.x);
+  public static void main(String [] args) {
+    Main mynum = new Main();
+    System.out.println(mynum.x + mynum.y);
   }
 }
