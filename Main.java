35020b68e419d389ca3dac628f76674536e1fe7d
subash <subash43it@proton.me>
2024-08-22 15:00:31 +0530

    ennum completed

--- Main.java
+++ Main.java
@@ -6,21 +6,8 @@ enum level {
 
 public class Main {
   public static void main(String[] args) {
-    level myvar = level.HIGH;
-
-    switch (myvar) {
-      case HIGH:
-        System.out.println("High level");
-        break;
-      case MEDIUM:
-        System.out.println("Medium level");
-        break;
-      case NORMAL:
-        System.out.println("Low level");
-        break;
-
-      default:
-        break;
+    for (level myVar1 : level.values()) {
+      System.out.println(myVar1);
     }
   }
 }
