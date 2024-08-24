fc2617d7130de0915326153440ff175effa45931
subash <subash43it@proton.me>
2024-08-24 14:29:49 +0530

    Iterator class, Iterationing the arraylist

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main {
-    public static void main(String[] args) {
-        HashMap<String,String> fn = new HashMap<String,String> ();
-        fn.put("dd", "dd");
-        fn.put("kjk", "kjskfk");
-        fn.put("kks", "klafjal");
-        fn.put("klasfal", "kfaslk");
-
-        fn.clear();
-
-    System.out.println(fn);
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    numbers.add(12);
+    numbers.add(8);
+    numbers.add(2);
+    numbers.add(23);
+    Iterator<Integer> it = numbers.iterator();
+    while(it.hasNext()) {
+      Integer i = it.next();
+      if(i < 10) {
+        it.remove();
+      }
     }
+    System.out.println(numbers);
+  }
 }
