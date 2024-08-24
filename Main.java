ad312fe2d38f22e4f88261ab5795d14b4f4c0997
subash <subash43it@proton.me>
2024-08-24 08:20:28 +0530

    make for loop for display the one by one element on the console.

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
 import java.util.ArrayList;
 
 public class Main {
-
   public static void main(String[] args) {
-    ArrayList<String> cars = new ArrayList<String>();
-    cars.add("volvo");
-    cars.add("BMW");
-    cars.add("Ford");
-
-    // finding the how many elements in the array.
-
-    System.out.println(cars.size());
+    ArrayList<String> myarray = new ArrayList<String>();
+    myarray.add("demo");
+    myarray.add("sdk");
+    myarray.add("funz");
+    for (int i = 0; i < myarray.size(); i++) {
+      System.out.println(myarray.get(i));
+    }
   }
 }
