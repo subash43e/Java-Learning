e3e32ce60d308c0f2ce3d450fdcc7dcbf288e6fc
subash <subash43it@proton.me>
2024-08-24 09:15:27 +0530

    Sorting Number with collections class and sort method.

--- Main.java
+++ Main.java
@@ -3,14 +3,14 @@ import java.util.Collections;
 
 public class Main {
   public static void main(String[] args) {
-    ArrayList<String> myArray = new ArrayList<String>();
-    myArray.add("z");
-    myArray.add("a");
-    myArray.add("g");
-
+    ArrayList<Integer> myArray = new ArrayList<Integer>();
+  myArray.add(4);
+  myArray.add(5);
+  myArray.add(3);
+  myArray.add(2);
     Collections.sort(myArray);
 
-    for (String d : myArray) {
+    for (int d : myArray) {
       System.out.println(d);
     }
   }
