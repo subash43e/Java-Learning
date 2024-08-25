31d623643cd481427ef5524ed2229ee0fded026b
subash <subash43it@proton.me>
2024-08-25 09:27:24 +0530

    Java Iterator creating && looping to the while loop

--- Main.java
+++ Main.java
@@ -3,18 +3,18 @@ import java.util.Iterator;
 
 public class Main {
   public static void main(String[] args) {
-    ArrayList<Integer> numbers = new ArrayList<Integer>();
-    numbers.add(12);
-    numbers.add(8);
-    numbers.add(2);
-    numbers.add(23);
-    Iterator<Integer> it = numbers.iterator();
-    while(it.hasNext()) {
-      Integer i = it.next();
-      if(i < 10) {
-        it.remove();
-      }
+    ArrayList<String> cars = new ArrayList<String>();
+
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+
+    Iterator<String> my = cars.iterator();
+
+    while (my.hasNext()) {
+      System.out.println(my.next());
     }
-    System.out.println(numbers);
+    System.out.println(my.next());
   }
 }
