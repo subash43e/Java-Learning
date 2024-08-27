e113d9beaa4117e938d0a20ddd6fb4c06854cf57
subash <subash43it@proton.me>
2024-08-27 21:44:01 +0530

    finding even number or odd even number

--- Main.java
+++ Main.java
@@ -1,25 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        int ages[] = { 20, 22, 18, 35, 48, 26, 87, 70 };
+        int number = 10;
 
-        float avg, sum = 0;
-
-        int length = ages.length;
-
-        int lowestAge = ages[0];
-
-        for (int age : ages) {
-            // The condition checking the lowestage greater than current age.
-            if (lowestAge > age) {
-
-                // Assigning the lowest age in lowestAge.
-                lowestAge = age;
-            }
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
         }
-
-        avg = sum / length;
-
-        System.out.println("The average age is: " + avg);
     }
 }
