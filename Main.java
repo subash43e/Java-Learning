e3fefaa169676f87d515845db8d92aa7ebd0c18f
subash <subash43it@proton.me>
2024-08-27 21:46:21 +0530

    Finding positive number and negative number

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        int number = 10;
+        int number = -10;
 
-        if (number % 2 == 0) {
-            System.out.println(number + " is even.");
+        if (number > 0) {
+            System.out.println(number + " is Positive number.");
         } else {
-            System.out.println(number + " is odd.");
+            System.out.println(number + " is Negative number.");
         }
     }
 }
