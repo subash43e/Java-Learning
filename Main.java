3d751736c408d0909a144bf040c91776693b2071
subash <subash43it@proton.me>
2024-08-28 12:00:22 +0530

    Fibonacci Series using recursion in java

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
 
 public class Main {
-    public static void main(String[] args) {
-        int n1 = 0, n2 = 1, n3, i, count = 10;
 
-        System.out.println(n1 + " " + n2);
-        for (i = 2; i < count; i++) {
+    static int n1 = 0, n2 = 1, n3;
+
+    static void fibonaci(int count) {
+        if (count > 0) {
             n3 = n1 + n2;
-            System.out.println(" " + n3);
             n1 = n2;
             n2 = n3;
+            System.out.println(" " + n3);
+            fibonaci(count - 1);
         }
+    }
 
+    public static void main(String[] args) {
+        int count = 10;
+        System.out.println(n1 + " " + n2);
+        fibonaci(count - 2);
     }
 }
